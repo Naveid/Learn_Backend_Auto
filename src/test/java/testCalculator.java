f004db83442aa350c601d0a1e65724453bf3a1ac
Mohammed Naveid <mnaveid@BLRC02TM09RG8WN.sea.corp.expecn.com>
2018-05-31 14:34:57 +0530

    Practice grouping

--- src/test/java/testCalculator.java
+++ src/test/java/testCalculator.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 public class testCalculator {
     Calculator cal = new Calculator();
 
-    @Test(groups = {"calc"})
+    @Test(groups = {"pos","all"})
     public void sumTest(){
         Assert.assertEquals(8,cal.sum(3,5),"failed");
     }
-    @Test(groups = {"calc"})
+    @Test(groups = {"pos","all"})
     public void diffTest(){
         Assert.assertEquals(2,cal.diff(5,3),"failed");
     }
-    @Test(groups = {"calc","nav"})
+    @Test(groups = {"neg","all"})
     public void mulTest(){
         Assert.assertEquals(15,cal.mul(3,5),"failed");
     }
-    @Test(groups = {"calc"})
+    @Test(groups = {"neg","all"})
     public void divTest(){
         Assert.assertEquals(2,cal.div(10,5),"failed");
     }
-    @Test(groups = {"calc"})
+    @Test(groups = {"neg","all"})
     public void modTest(){
         Assert.assertEquals(1,cal.mod(11,5),"failed");
     }
