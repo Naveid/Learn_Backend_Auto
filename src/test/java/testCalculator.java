ef307321df31c97e923eb24771c5cccae83ba067
Mohammed Naveid <mnaveid@BLRC02TM09RG8WN.sea.corp.expecn.com>
2018-06-01 13:01:49 +0530

    running test with TestNG

--- src/test/java/testCalculator.java
+++ src/test/java/testCalculator.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 public class testCalculator {
     Calculator cal = new Calculator();
 
-    @Test(groups = {"pos","all"})
+    @Test(groups = {"pos"})
     public void sumTest(){
         Assert.assertEquals(8,cal.sum(3,5),"failed");
     }
-    @Test(groups = {"pos","all"})
+    @Test(groups = {"pos"})
     public void diffTest(){
         Assert.assertEquals(2,cal.diff(5,3),"failed");
     }
-    @Test(groups = {"neg","all"})
+    @Test(groups = {"neg"})
     public void mulTest(){
         Assert.assertEquals(15,cal.mul(3,5),"failed");
     }
-    @Test(groups = {"neg","all"})
+    @Test(groups = {"neg"})
     public void divTest(){
         Assert.assertEquals(2,cal.div(10,5),"failed");
     }
-    @Test(groups = {"neg","all"})
+    @Test(groups = {"neg"})
     public void modTest(){
         Assert.assertEquals(1,cal.mod(11,5),"failed");
     }
