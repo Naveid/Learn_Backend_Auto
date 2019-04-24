033e2c9b1ad2dab16080ac111b40a4f872a3ba66
Mohammed Naveid <mnaveid@BLRC02TM09RG8WN.sea.corp.expecn.com>
2019-04-24 12:49:35 +0530

    changes to class2

--- src/test/java/testClass2.java
+++ src/test/java/testClass2.java
@@ -3,12 +3,20 @@ import org.testng.annotations.Test;
 
 public class testClass2 {
     Calculator cal = new Calculator();
+
     @Test(groups = {"sanity"})
-    public void divTest(){
-        Assert.assertEquals(2,cal.div(10,5),"failed");
+    public void divTest() {
+        Assert.assertEquals(2, cal.div(10, 5), "failed");
     }
+
     @Test(groups = {"reg"})
-    public void modTest(){
-        Assert.assertEquals(1,cal.mod(11,5),"failed");
+    public void modTest() {
+        Assert.assertEquals(1, cal.mod(11, 5), "failed");
+    }
+
+    @Test(groups = {"reg"})
+    public void mulTest() {
+        Assert.assertEquals(15, cal.mul(3, 5), "failed");
+        System.out.println("in class2");
     }
 }
