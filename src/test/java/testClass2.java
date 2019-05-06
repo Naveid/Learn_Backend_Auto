c70ae1f5190d2df8f6817e9bdb04f03944907ed0
Mohammed Naveid <mnaveid@BLRC02TM09RG8WN.sea.corp.expecn.com>
2019-05-06 15:15:25 +0530

    adding logs

--- src/test/java/testClass2.java
+++ src/test/java/testClass2.java
@@ -7,16 +7,18 @@ public class testClass2 {
     @Test(groups = {"sanity"})
     public void divTest() {
         Assert.assertEquals(2, cal.div(10, 5), "failed");
+        System.out.println("in class2 sanity");
     }
 
     @Test(groups = {"reg"})
     public void modTest() {
         Assert.assertEquals(1, cal.mod(11, 5), "failed");
+        System.out.println("in class2 regression");
     }
 
     @Test(groups = {"reg"})
     public void mulTest() {
         Assert.assertEquals(15, cal.mul(3, 5), "failed");
-        System.out.println("in class2");
+        System.out.println("in class2 regression");
     }
 }
