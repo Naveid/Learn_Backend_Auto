3b74cdc72a78ce09427e918c82ad76dbd6e68d9d
mnaveid <mohammednaveid@gmail.com>
2020-03-05 14:54:18 +0530

    Page factory model implemented

--- src/test/java/Sel.java
+++ src/test/java/Sel.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.*;
 
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -12,7 +14,21 @@ import java.util.concurrent.TimeUnit;
 
 
 public class Sel {
-    private WebDriver driver;
+     private WebDriver driver;
+
+    @FindBy(xpath = "//*[@id=\"qf-0q-destination\"]")
+    WebElement locationTextBox;
+    @FindBy(id = "citysqm-asi0-s0")
+    WebElement priceDropDown;
+    @FindBy(xpath="//*[@id=\"hds-marquee\"]/div[2]/div[1]/div/form/div[4]/button")
+    WebElement landingSubmit;
+    @FindBy(xpath="//*[@id=\"enhanced-sort\"]/li[5]/a")
+    WebElement sAPriceSortDropdown;
+    @FindBy(xpath="//*[@id=\"sort-submenu-price\"]/li[1]/a")
+    WebElement sAPriceSortList;
+
+
+
     @Test(groups={"uitest"})
     public void testEasy() {
         driver.get("http://demo.guru99.com/test/guru99home/");
@@ -22,32 +38,27 @@ public class Sel {
     }
     @Test(groups={"uitest"})
     public void hcom() throws InterruptedException {
+      //  Hcompage abc = new Hcompage(driver);         This to be used if we want to use page factory elements from another class having page elements
+        PageFactory.initElements(driver,this);
         driver.manage().window().setSize(new Dimension(1600,900));
         driver.get("https://www.staging1-hotels.com/");
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         WebDriverWait wait = new WebDriverWait(driver, 4);
-        WebElement abc = driver.findElement(By.xpath("//*[@id=\"qf-0q-destination\"]"));
-        abc.sendKeys("lond");
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("citysqm-asi0-s0")));
-        WebElement def = driver.findElement(By.id("citysqm-asi0-s0"));
-        def.click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"hds-marquee\"]/div[2]/div[1]/div/form/div[4]/button")));
-        WebElement kkk = driver.findElement(By.xpath("//*[@id=\"hds-marquee\"]/div[2]/div[1]/div/form/div[4]/button"));
-        kkk.click();
-        Thread.sleep(2000);
-        System.out.println(driver.getCurrentUrl());
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"enhanced-sort\"]/li[5]/a")));
-        WebElement priceLink=driver.findElement(By.xpath("//*[@id=\"enhanced-sort\"]/li[5]/a"));
-        priceLink.click();
-        WebElement bestval=driver.findElement(By.xpath("//*[@id=\"sort-submenu-price\"]/li[1]/a"));
-        String bestvalText=bestval.getText();
+        locationTextBox.sendKeys("lond");
+        wait.until(ExpectedConditions.elementToBeClickable(priceDropDown));
+        priceDropDown.click();
+        wait.until(ExpectedConditions.elementToBeClickable(landingSubmit));
+        landingSubmit.click();
+        wait.until(ExpectedConditions.elementToBeClickable(sAPriceSortDropdown));
+        sAPriceSortDropdown.click();
+        String bestvalText=sAPriceSortList.getText();
         if(bestvalText.contains("Best")|| bestvalText.contains("Lowest")){
-            bestval.click();
+            sAPriceSortList.click();
             System.out.println(bestvalText+" clicked");
         }
         System.out.println(bestvalText);
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"enhanced-sort\"]/li[5]/a")));
+        wait.until(ExpectedConditions.elementToBeClickable(sAPriceSortDropdown));
 
     }
     @BeforeTest
